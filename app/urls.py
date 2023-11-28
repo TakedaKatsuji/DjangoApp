29ea4561dc205303ea82706d728b82c0e4bd11c7
TakedaKatsuji <katsuji.takeda@riken.jp>
2023-11-28 22:08:10 +0900

    add

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .views import TestView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'practice', TestView.as_view(), name='testview')
 ]
